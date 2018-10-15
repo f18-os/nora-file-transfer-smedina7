3bc96ca5d10801b88ce2cee0c5f4c4edc8b3290e
Stephanie <smedina7@miners.utep.edu>
2018-10-15 10:27:07 -0600

    Fixing indents

--- emphaticDemo/framedThreadClient.py
+++ emphaticDemo/framedThreadClient.py
@@ -10,19 +10,17 @@ import time
 
 switchesVarDefaults = (
     (('-s', '--server'), 'server', "localhost:50001"),
-    (('-d', '--debug'), "debug", False), # boolean (set if present)
-    (('-?', '--usage'), "usage", False), # boolean (set if present)
+    (('-d', '--debug'), "debug", False),  # boolean (set if present)
+    (('-?', '--usage'), "usage", False),  # boolean (set if present)
 )
-
 
 progname = "framedClient"
 paramMap = params.parseParams(switchesVarDefaults)
 
-server, usage, debug  = paramMap["server"], paramMap["usage"], paramMap["debug"]
+server, usage, debug = paramMap["server"], paramMap["usage"], paramMap["debug"]
 
 if usage:
     params.usage()
-
 
 try:
     serverHost, serverPort = re.split(":", server)
@@ -31,65 +29,65 @@ except:
     print("Can't parse server:port from '%s'" % server)
     sys.exit(1)
 
+
 class ClientThread(Thread):
     def __init__(self, serverHost, serverPort, debug):
         Thread.__init__(self, daemon=False)
         self.serverHost, self.serverPort, self.debug = serverHost, serverPort, debug
         self.start()
+
     def run(self):
-       s = None
-       for res in socket.getaddrinfo(serverHost, serverPort, socket.AF_UNSPEC, socket.SOCK_STREAM):
-           af, socktype, proto, canonname, sa = res
-           try:
-               print("creating sock: af=%d, type=%d, proto=%d" % (af, socktype, proto))
-               s = socket.socket(af, socktype, proto)
-           except socket.error as msg:
-               print(" error: %s" % msg)
-               s = None
-               continue
-           try:
-               print(" attempting to connect to %s" % repr(sa))
-               s.connect(sa)
-           except socket.error as msg:
-               print(" error: %s" % msg)
-               s.close()
-               s = None
-               continue
-           break
+        s = None
+        for res in socket.getaddrinfo(serverHost, serverPort, socket.AF_UNSPEC, socket.SOCK_STREAM):
+            af, socktype, proto, canonname, sa = res
+            try:
+                print("creating sock: af=%d, type=%d, proto=%d" % (af, socktype, proto))
+                s = socket.socket(af, socktype, proto)
+            except socket.error as msg:
+                print(" error: %s" % msg)
+                s = None
+                continue
+            try:
+                print(" attempting to connect to %s" % repr(sa))
+                s.connect(sa)
+            except socket.error as msg:
+                print(" error: %s" % msg)
+                s.close()
+                s = None
+                continue
+            break
 
-       if s is None:
-           print('could not open socket')
-           sys.exit(1)
+        if s is None:
+            print('could not open socket')
+            sys.exit(1)
 
-       clientFile = input("What file do you want to send:\n")
+        clientFile = input("What file do you want to send:\n")
 
+        if not os.path.exists(clientFile):
+            print("File %s doesn't exist! Exiting" % clientFile)
+            sys.exit(1)
 
-       if not os.path.exists(clientFile):
-           print ("File %s doesn't exist! Exiting" % clientFile)
-           sys.exit(1)
+        fs = FramedStreamSock(s, debug=debug)
 
-       fs = FramedStreamSock(s, debug=debug)
-       #sending name of file first so that server can verify
+        # sending name of file first so that server can verify
+        print("sendind file: " + clientFile)
+        fs.sendmsg(clientFile)
 
-       print("sendind file: " + clientFile)
-       fs.sendmsg(clientFile)
-
-       #if file exits..
-       if(fs.receivemsg() == b"ERROR File already exists... Exiting."):
-           print(fs.receivemsg())
-           sys.exit(1)  #exit
+        # if file exits..
+        if (fs.receivemsg() == b"ERROR File already exists... Exiting."):
+            print(fs.receivemsg())
+            sys.exit(1)  # exit
 
         # #if server says it's ready
-       if(fs.receivemsg() == b"Ready"):
+        if (fs.receivemsg() == b"Ready"):
             print("Sending...")
 
-       f = open(clientFile, "rb")
-       byte = f.read(100)
-       while(byte):
-           fs.sendmsg(byte)
-           print("Sending copy of...", fs.receivemsg())
-           byte = f.read(100)
-
+        f = open(clientFile, "rb")
+        byte = f.read(100)
+        while (byte):
+            fs.sendmsg(byte)
+            print("Sending copy of...", fs.receivemsg())
+            byte = f.read(100)
 
 
 for i in range(100):
